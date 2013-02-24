4d320b8e8861a60c5f992a5ad60f4fff3c643664
csigi <trannguyenbaotrung@gmail.com>
2013-02-25 03:10:37 +0900

    Finish sign in (refactor form_for to form_tag)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # http_basic_authenticate_with name: "frodo", password: "thering"
   include SessionsHelper
 
   helper_method :get_client_ip
