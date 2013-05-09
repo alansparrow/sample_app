0eb9f0fc3d235bfd5258cc473e8b678899486b8f
CSIGI <trannguyenbaotrung@gmail.com>
2013-05-10 04:04:37 +0900

    Add name to greeting email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "hi@sagwija.com"
 
   def registration_confirmation(user)
+  	@user = user
   	mail(:to => user.email, :subject => "Registered")
   end
 end
