1913652b052fe53c308ef7cd54d079ed68df0008
CSIGI <trannguyenbaotrung@gmail.com>
2013-05-14 21:58:58 +0900

    Fix title to Unicorn Fellowship

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis
 	def full_title(page_title)
-		base_title = "Unicorn Fellow"
+		base_title = "Unicorn Fellowship"
 		if page_title.empty?
 			base_title
 		else
